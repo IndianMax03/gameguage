a58e4ec01787cd963ac7c58c01a069b7cfee614d
Anth0o0ny <pushkin.a.s.03@mail.ru>
2024-04-22 22:59:38 +0300

    Update: rus to eng

--- src/main.py
+++ src/main.py
@@ -8,26 +8,26 @@ logging.basicConfig(level=logging.INFO)
 
 @dp.message_handler(commands=['start'])
 async def start(message: types.Message):
-    await message.reply("Привет! Я бот. Напиши /help для списка команд.")
+    await message.reply("Hi! I am a bot. Write /help for the command list.")
 
 @dp.message_handler(commands=['help'])
 async def help_command(message: types.Message):
-    await message.reply("Список доступных команд:\n"
-                        "/help - показать список команд\n"
-                        "/game1 - запустить игру 1\n"
-                        "/game2 - запустить игру 2\n"
-                        "/game3 - запустить игру 3\n"
-                        "/game4 - запустить игру 4\n"
-                        "/game5 - запустить игру 5")
+    await message.reply("List of available commands:\n"
+                        "/help - show command list\n"
+                        "/game1 - start game 1\n"
+                        "/game2 - start game 2\n"
+                        "/game3 - start game 3\n"
+                        "/game4 - start game 4\n"
+                        "/game5 - start game 5")
 
 
 @dp.message_handler(commands=['game1'])
 async def game1(message: types.Message):
-    await message.reply("Это игра 1")
+    await message.reply("This is game 1")
 
 @dp.message_handler(commands=['game2'])
 async def game2(message: types.Message):
-    await message.reply("Это игра 2")
+    await message.reply("This is game 2")
 
 
 if __name__ == '__main__':
